c7e3c976740c74ef5921cc18c5adb676a851382d
Jan Schill <janschill@me.com>
2020-07-25 23:49:29 +0200

    Add dependencies for random, time

--- app/Main.hs
+++ app/Main.hs
@@ -1,10 +1,18 @@
 module Main where
 
+import Data.Time.Clock.POSIX (getPOSIXTime)
+import Foreign.Marshal.Unsafe (unsafeLocalState)
 import Rng
 import System.Random
 
+randomNumber :: Int
+randomNumber = do
+  let currentTimeStamp = (round . (* 1000)) <$> getPOSIXTime -- get current time as UNIX timestamp
+  let seed = mkStdGen $ unsafeLocalState currentTimeStamp -- IO Int -> Int
+  let (number, nextGen) = getRandom 1 6 seed
+  number
+
 main :: IO ()
 main = do
-  let init = mkStdGen 11
-  let (number, nextGen) = getRandom 1 6 init
-  print number
+  print "Random number is: "
+  print randomNumber
